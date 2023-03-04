77254a190d06476c88c87adb7e5ef80b8466db74
Louis Jarvis <ljar069@aucklanduni.ac.nz>
2023-03-05 11:51:37 +1300

    add documentation for functions

--- R/read-wikipedia.R
+++ R/read-wikipedia.R
@@ -5,16 +5,12 @@
 # Return the url
 
 
-#TODO wrap in a try execpt
-main_page <- rvest::read_html("https://en.wikipedia.org/wiki/Main_Page")
-
-today_list_str <- main_page %>% 
-  rvest::html_element("#mp-otd") %>% # use css selectors to obtain div
-  rvest::html_text() #[A-za-z0-9]
-
-# remove the "More anniversaries" onwards section
-##str_end <- stringr::str_length(today_list_str)
-
+#' Drop all text starting at "More anniversaries"
+#'
+#' @param paragraph_str character vector, the wikipedia page content as a single string
+#'
+#' @return a string with all text between "More anniversaries" and the end excluded
+#' @export
 drop_more_anniveraries <- function(paragraph_str) {
   
   anni_start <- paragraph_str %>% 
@@ -27,31 +23,91 @@ drop_more_anniveraries <- function(paragraph_str) {
   return(str_stripped)
 }
 
-today_list_vec <- today_list_str %>%
-  drop_more_anniveraries() %>%
-  stringr::str_replace(. , pattern = "(?<=[a-z])\\.(?=[A-Z])", replacement = ".\n") %>% # this stops multiple sentences being put on the same line e.g. foo.Bar -- > foo.\nBar
-  stringr::str_split(pattern = "\n") %>%
-  purrr::pluck(1) %>%
-  base::Filter(f = function(line) ifelse(line == "", F, T), x = .)
 
-# drop empty string elements ("")
-
-# pull out the date
-date_day <- today_list_vec[1]
-
+#' Convert the yyyy - event pairs into tibble rows
+#'
+#' @param event_str list of character vectors, each containing a yyyy - event pair
+#' e.g. 2012 – A series of blasts occurred at an arms dump ...
+#'
+#' @return a dataframe with columns: `Year` `and Details``
+#' @export
 events_to_table <- function(event_str) {
   
-   ##if (stringr::str_detect(pattern = "[0-9+] – ")) {
-     
-     event_row <- tibble::tibble(
-       Year = stringr::str_extract(event_str, pattern = '(\\d{4})'),
-       Details = stringr::str_split(event_str, pattern = " – ")[[1]][2]
-     )
+  event_row <- tibble::tibble(
+    Year = stringr::str_extract(event_str, pattern = '(\\d{4})'),
+    Details = stringr::str_split(event_str, pattern = " – ")[[1]][2]
+  )
   
   return(event_row)
 }
 
-# today_list_vec %>%
-#   base::Filter(f = function(x) stringr::str_detect(x, pattern = "\\d{4} – ")) %>%
-#   purrr::map_df(.f = events_to_table) %>%
-#   dplyr::bind_rows()
+#' Convert list of strings into a tibble of events and details
+#'
+#' @param events_list list of characters
+#'
+#' @return tbl
+#' @export
+create_events_table <- function(events_list) {
+  
+  event_tbl <- events_list %>%
+    base::Filter(f = function(x) stringr::str_detect(x, pattern = "\\d{4} – ")) %>%
+    purrr::map_df(.f = events_to_table) %>%
+    dplyr::bind_rows() 
+  
+  return(event_tbl)
+}
+
+
+#TODO wrap in a try execpt
+
+#' Read in Wikipedia html and convert to string
+#'
+#' @return character vector with all the wikipedia contents
+#' @export
+
+read_wiki_html <- function() {
+  main_page <- rvest::read_html("https://en.wikipedia.org/wiki/Main_Page")
+  
+  today_list_str <- main_page %>% 
+    rvest::html_element("#mp-otd") %>% # use css selectors to obtain div
+    rvest::html_text() 
+  
+  return(today_list_str)
+}
+
+# remove the "More anniversaries" onwards section
+##str_end <- stringr::str_length(today_list_str)
+
+
+
+#' split large string into list of sentences
+#'
+#' @param today_list_str character vector
+#'
+#' @return list of characters, each a different sentence
+#' @export
+text_to_vec <- function(today_list_str) {
+  
+  # this stops multiple sentences being put on the same line e.g. 
+  # foo.Bar -- > foo.\nBar, which can then be split into multiple lines
+  
+  today_list_vec <- today_list_str %>%
+    drop_more_anniveraries() %>%
+    stringr::str_replace(
+      string = .,
+      pattern = "(?<=[a-z])\\.(?=[A-Z])",
+      replacement = ".\n"
+    ) %>%
+    stringr::str_split(pattern = "\n") %>%
+    purrr::pluck(1) %>%
+    base::Filter(f = function(line) dplyr::ifelse(line == "", F, T), x = .) 
+  
+  return(today_list_vec)
+}
+
+
+# drop empty string elements ("")
+
+# pull out the date
+#date_day <- today_list_vec[1]
+
