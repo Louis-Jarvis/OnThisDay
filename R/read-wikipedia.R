9fee8476d3df4db161e5278430e3d8cd79aa8e8a
Louis Jarvis <ljar069@aucklanduni.ac.nz>
2023-03-05 16:47:02 +1300

    add colour to titles

--- R/read-wikipedia.R
+++ R/read-wikipedia.R
@@ -39,20 +39,20 @@ get_daily_facts <- function() {
   
   events_list <- read_wiki_html() %>% text_to_event_list()
   
-  cli::cli_h1("Guess What Happened On This Day!")
+  cli::cli_h1(cli::col_green("Guess What Happened On This Day!"))
   ##print(glue::glue("{events_list[1]} {substr(Sys.Date(), 1, 4)}"))
   
-  cli::cli_h2("Festivals / National Days of Importance / Holidays")
+  cli::cli_h2(cli::col_cyan("Festivals / National Days of Importance / Holidays"))
   cli::cli_text(events_list[1])
   ##TODO print the festival string
   
-  cli::cli_h2("On This Day...")
+  cli::cli_h2(cli::col_cyan("On This Day..."))
   events_tbl <- events_list %>% create_events_table() 
   
   print(events_tbl)
   cli::cli_text("")
   
-  cli::cli_h2("Famous People")
+  cli::cli_h2(cli::col_cyan("Famous People"))
   famous_ppl_tbl <- events_list %>% create_births_deaths_table()
   
   print(famous_ppl_tbl)
