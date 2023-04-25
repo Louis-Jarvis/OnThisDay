aa7fb00c0e05bb05173a9b10f6fe3631855b3d06
Louis Jarvis <ljar069@aucklanduni.ac.nz>
2023-04-25 21:31:34 +1200

    fix bug in print_national_days

--- R/days_of_importance.R
+++ R/days_of_importance.R
@@ -1,11 +1,15 @@
 
 print_national_days <- function(events_list) {
-  # National Holidays
-  national_days <- stringr::str_split_i(events_list[1], ":", 2)
+  national_days <- stringr::str_split(events_list[1], ":|;")[[1]]
   
-  if(!is.na(national_days)) {
+  if(!all(is.na(national_days))) {
+    nat_days_list <- national_days[ 2 : length(national_days)]
+    
     h2_cyan("Festivals / National Days of Importance / Holidays")
-    nat_days_list <- stringr::str_split_i(national_days, ";", 1) %>% trimws()
-    pretty_list(nat_days_list, function(txt) cli::cli_li(italic_li(txt)))
+    
+    pretty_list(nat_days_list, function(txt)
+      cli::cli_li(italic_li(txt)))
   }
+  
+  return(invisible(NULL))
 }
